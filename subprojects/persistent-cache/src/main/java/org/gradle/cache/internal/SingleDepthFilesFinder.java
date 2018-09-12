721c8d46aa7fdfdbea9a8e89a0252dd3f6801cda
Marc Philipp <marc@gradle.com>
2018-09-12 12:10:49 +0200

    Update Guava to 26.0
     - Replace `Files.readLines()` with `Files.asCharSource().readLines()`
     - Replace `Files.write()` with `Files.asCharSink().write()`
     - Change `Objects.toStringHelper()` to `MoreObjects.toStringHelper()`
     - Change `Iterators.emptyIterator()` to `Collections.emptyIterator()`
     - Use `MoreExecutors.directExecutor()` with `Futures.addCallback`
     - Replace `CharMatcher.JAVA_UPPER_CASE`/`JAVA_ISO_CONTROL` with
       `CharMatcher.javaUpperCase()`/`CharMatcher.javaIsoControl()`
     - Replace `new SimpleTimeLimiter()` with `SimpleTimeLimiter.create()`
     - Update `DistributionIntegrationSpec` because Guava requires 4
       additional downstream dependencies:
        * org.checkerframework:checker-compat-qual
        * com.google.errorprone:error_prone_annotations
        * com.google.j2objc:j2objc-annotations
        * org.codehaus.mojo:animal-sniffer-annotations

--- subprojects/persistent-cache/src/main/java/org/gradle/cache/internal/SingleDepthFilesFinder.java
+++ subprojects/persistent-cache/src/main/java/org/gradle/cache/internal/SingleDepthFilesFinder.java
@@ -24,6 +24,7 @@ import javax.annotation.Nonnull;
 import java.io.File;
 import java.io.FileFilter;
 import java.util.ArrayDeque;
+import java.util.Collections;
 import java.util.Deque;
 import java.util.Iterator;
 
@@ -82,9 +83,10 @@ public class SingleDepthFilesFinder implements FilesFinder {
             return stack.size() == targetSize && stack.getLast().hasNext();
         }
 
+        @SuppressWarnings("Since15")
         private Iterator<File> listFiles(File baseDir) {
             File[] files = baseDir.listFiles(filter);
-            return files == null ? Iterators.<File>emptyIterator() : Iterators.forArray(files);
+            return files == null ? Collections.<File>emptyIterator() : Iterators.forArray(files);
         }
     }
 }
